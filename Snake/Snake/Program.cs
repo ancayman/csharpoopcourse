19127c5c629ba06aaacf3523a0e3eb71f92d07a4
Андрей Кайманов < >
2016-03-06 21:54:11 +0300

    Класс Горизонтальные линии.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,42 +10,14 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            HorizontalLine lineH1 = new HorizontalLine(5, 10, 7, '+');
+            lineH1.Drow();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            VerticalLine lineV1 = new VerticalLine(5, 10, 11, '+');
+            lineV1.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            //Домашнее задание:
-            List<Char> chList = new List<char>();
-            chList.Add('$');
-            chList.Add('&');
-
-            List<Point> p2List = new List<Point>();
-            Point p3 = new Point(5, 5, '$');
-            Point p4 = new Point(10, 10, '&');
-            p2List.Add(p3);
-            p2List.Add(p4);
+            VerticalLine lineV2 = new VerticalLine(5, 10, 4, '+');
+            lineV2.Drow();
 
             Console.ReadLine();
         }
