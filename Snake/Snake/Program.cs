cff412133092dc15f3c954c0a05b8dba87e2e2a4
Андрей Кайманов < >
2016-03-06 21:28:38 +0300

    Реализация класса List.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,61 +10,44 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x = 1;
-            Func1(x);
-            Console.WriteLine("Call Func1. x = " + x);
-
-            x = 1;
-            Func2(x);
-            Console.WriteLine("Call Func2. x = " + x);
-
-            x = 1;
-            Func3(x);
-            Console.WriteLine("Call Func3. x = " + x);
-
             Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+            p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            p1 = p2;
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
+            p2.Draw();
 
-            p1 = new Point(1, 3, '*');
-            Update(p1);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            //Домашнее задание:
+            List<Char> chList = new List<char>();
+            chList.Add('$');
+            chList.Add('&');
+
+            List<Point> p2List = new List<Point>();
+            Point p3 = new Point(5, 5, '$');
+            Point p4 = new Point(10, 10, '&');
+            p2List.Add(p3);
+            p2List.Add(p4);
 
             Console.ReadLine();
         }
-
-        public static void Func1(int value)
-        {
-
-        }
-
-        public static void Func2(int value)
-        {
-            value = value + 1;
-        }
-
-        public static void Func3(int x)
-        {
-            x = x + 1;
-        }
-
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x = p.x + dx;
-            p.y = p.y + dy;
-        }
-
-        public static void Update(Point p)
-        {
-            p = new Point();
-        }
-
-
     }
 }
