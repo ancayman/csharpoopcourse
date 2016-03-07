2c55513929f1ffd425f866b5e4792afdb871d67f
Андрей Кайманов < >
2016-03-07 17:13:41 +0300

    Добавлен класс Создание еды. Реализован метод поедания еды и увеличение змейки.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -23,21 +22,34 @@ namespace Snake
             leftLine.Drow();
             rightLine.Drow();
 
-
             // Отрисовка точек			
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
         }
     }
